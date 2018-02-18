e7fb1db0a39189e938808532962ada93f7d633f8
farrell <farrell@adobe.com>
2018-02-18 14:33:44 -0800

    Recreate click handler logic in new calc-app tag
    To use React's setState properly, create a simple loop to overlay values on top of existing object
    
    The App React Component virtually has nothing in it now

--- src/component/App.js
+++ src/component/App.js
@@ -1,39 +1,12 @@
 import React from 'react';
 //import Display from './Display';
 //import ButtonPanel from './ButtonPanel';
-import CalcButtonPanel from '../webcomponents/calcbuttonpanel/calcbuttonpanel.js';
-import CalcButton from '../webcomponents/calcbutton/calcbutton.js';
-import CalcDisplay from '../webcomponents/calcdisplay/calcdisplay.js';
-import calculate from '../logic/calculate';
-import './App.css';
+import CalcButtonPanel from '../webcomponents/calcapp/calcapp.js';
+//import './App.css';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            total: null,
-            next: null,
-            operation: null,
-        };
-    }
-
-    componentDidMount() {
-        this.buttonPanelEl.addEventListener(CalcButton.CALC_BUTTON_PRESS, e => this.handleClick(e.detail.name));
-        this.displayEl = this.state.next || this.state.total || '0';
-    }
-
-    handleClick = (buttonName) => {
-        this.setState(calculate(this.state, buttonName));
-        this.displayEl.value = this.state.next || this.state.total || '0';
-    }
-
     render() {
-        return (
-            <div className="component-app">
-                <calc-display ref={(el) => { this.displayEl = el; }}></calc-display>
-                <calc-buttonpanel ref={(el) => { this.buttonPanelEl = el; }} />
-            </div>
-        );
+        return (<calc-app></calc-app>);
     }
 }
 export default App;
---
+++ src/webcomponents/calcapp/calcapp.js
@@ -0,0 +1,45 @@
+// Webpack just bundles this in, no problem - but typcially module imports don't work with CSS
+import '../../component/App.css';
+
+import CalcDisplay from '../calcdisplay/calcdisplay.js';
+import CalcButtonPanel from '../calcbuttonpanel/calcbuttonpanel.js';
+import CalcButton from '../calcbutton/calcbutton.js';
+import Calculate from '../../logic/calculate.js';
+
+export default class CalcApp extends HTMLElement {
+    constructor() {
+        super();
+        this.state = {
+            total: null,
+            next: null,
+            operation: null,
+        };
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+                <calc-display></calc-display>
+                <calc-buttonpanel></calc-buttonpanel>`;
+
+        this.classList.add('component-app');
+        this.dom = {
+            buttonpanel: this.querySelector('calc-buttonpanel'),
+            display: this.querySelector('calc-display')
+        };
+
+        this.dom.buttonpanel.addEventListener(CalcButton.CALC_BUTTON_PRESS, e => this.onButtonPress(e));
+    }
+
+    onButtonPress(event) {
+        // React's setState overlaid return values over the original - a simple for loop can do the same
+        let calc = Calculate(this.state, event.detail.name);
+        for (let c in calc) {
+            this.state[c] = calc[c];
+        }
+        this.dom.display.value = this.state.next || this.state.total || '0';
+    }
+}
+
+if (!customElements.get('calc-app')) {
+    customElements.define('calc-app', CalcApp);
+}
